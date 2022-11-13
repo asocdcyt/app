4b7a8d7031a159331b1caa58ef64ace42ddaa41c
asocdcyt <115439521+asocdcyt@users.noreply.github.com>
2022-11-13 11:03:09 -0800

    asdsads
    asdasdasd

--- src/config.js
+++ src/config.js
@@ -2,5 +2,9 @@ import {config} from 'dotenv';
 config();
 
 export default {
+<<<<<<< HEAD
     mongodbURL: process.env.MONGODB_URI || 'mongodb+srv://cool1:cool2@cluster0.3bmi5nw.mongodb.net/dbAsocDCyTon',
+=======
+    mongodbURL: process.env.MONGODB_URI || "mongodb+srv://cool1:cool2@cluster0.4oycgkx.mongodb.net/dbAsocDCyTon",
+>>>>>>> parent of b5c5419 (ASDFASDAS)
 };
